f9364f2317c6e6c89b2da7080fd350a8adb3f56d
Prince Kumar <110832286+singhprincekumar12006011@users.noreply.github.com>
2023-06-10 11:26:26 +0530

    Add files via upload

---
+++ Count_digit.java
@@ -0,0 +1,14 @@
+/*
+
+take interger an argument and count the digit.
+
+*/
+
+int CountDigit(int x){
+    int result = 0;
+    while(x>0){
+        x=x/10;
+        result++;
+    }
+    return result;
+}
